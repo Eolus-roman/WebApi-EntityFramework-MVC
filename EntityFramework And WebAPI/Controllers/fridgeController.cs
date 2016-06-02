ac363b286418a5a17b4ad3db8d2b6b12b7d337ea
Eolus-roman <eolus.roman@gmail.com>
2016-06-02 16:50:34 +0300

    try adding API

---
+++ EntityFramework And WebAPI/Controllers/fridgeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Smart_House;
+using EntityFramework_And_WebAPI.Models;
+
+namespace EntityFramework_And_WebAPI.Controllers
+{
+    public class fridgeController : ApiController
+    {
+        private DeviceContext db = new DeviceContext();
+        // api/fridge
+    
+        // PUT api/fridge/5
+        [Route("api/fridge/{button}/{id}")]
+        public string Put(int id, string button)
+        {
+            Device dev = db.Fridges.Find(id);
+            if (dev != null)
+            {
+
+                switch (button)
+                {
+                    case "on":
+                        dev.On();
+                        break;
+                }
+            }
+            db.SaveChanges();
+            return dev.ToString();
+        }
+
+        // DELETE api/fridge/5
+        public void Delete(int id)
+        {
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EntityFramework And WebAPI/Scripts/DevButt.js
@@ -0,0 +1,20 @@
+﻿function Action(key) {
+    $("#" + key + "-onFr").click(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: "api/fridge/on/" + key,
+            type: "PUT",
+            success: function (data) {
+                $("#" + key + "-status").html(data);
+            }
+
+        });
+    });
+
+
+
+
+
+
+
+}
